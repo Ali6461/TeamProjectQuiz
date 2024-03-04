eb84ff09ce157a6d1c5c05a611090ecb07814f4f
Akashi9006 <malif9006@gmail.com>
2024-03-04 12:03:50 +0000

    page1 changes

--- questions.js
+++ questions.js
@@ -1,5 +1,3 @@
-// script.js
-
 // Function to prompt the user for a question
 function askQuestion() {
     var userQuestion = prompt("Please enter your question:");
@@ -7,19 +5,9 @@ function askQuestion() {
 }
 
 // Function to prompt the user for an answer
-function askAnswer() {
-    var userAnswer = prompt("Please enter the correct answer:");
+function askAnswer(promptText) {
+    var userAnswer = prompt(promptText);
     return userAnswer;
-}
-
-// Function to prompt the user for multiple incorrect answers
-function askIncorrectAnswers() {
-    var incorrectAnswers = [];
-    for (var i = 0; i < 3; i++) {  // Prompt for three incorrect answers
-        var incorrectAnswer = prompt("Please enter an incorrect answer:");
-        incorrectAnswers.push(incorrectAnswer);
-    }
-    return incorrectAnswers;
 }
 
 // Function to save the question and answers to a JSON file
@@ -52,11 +40,17 @@ function saveToJSON(question, correctAnswer, incorrectAnswers) {
 // Function to handle the button click event
 function askQuestionsAndSave() {
     var userQuestion = askQuestion();
-    var userCorrectAnswer = askAnswer();
-    var userIncorrectAnswers = askIncorrectAnswers();
+    var userCorrectAnswer = askAnswer("Please enter the correct answer:");
+    var userIncorrectAnswer1 = askAnswer("Please enter an incorrect answer:");
+    var userIncorrectAnswer2 = askAnswer("Please enter another incorrect answer:");
+    var userIncorrectAnswer3 = askAnswer("Please enter one more incorrect answer:");
 
-    if (userQuestion && userCorrectAnswer && userIncorrectAnswers.length > 0) {
+    var userIncorrectAnswers = [userIncorrectAnswer1, userIncorrectAnswer2, userIncorrectAnswer3];
+
+    if (userQuestion && userCorrectAnswer && userIncorrectAnswers.every(Boolean)) {
         saveToJSON(userQuestion, userCorrectAnswer, userIncorrectAnswers);
         alert("Question and answers saved");
+    } else {
+        alert("Please fill in all fields.");
     }
 }
